084419a493257f42f2e1142c13a99d2d632ba8d3
Alaa Alaff <travel98_alaff@hotmail.com>
2021-08-01 05:19:31 +0300

    the second (last) col in WorkSpace component has been styled

--- src/components/WorkSpace.js
+++ src/components/WorkSpace.js
@@ -7,7 +7,7 @@ function WorkSpace() {
     <Container>
       <section className="raw mt-45 d-flex">
         <div className="col-sm-6 ">
-          <div className="mb-2 mb-3 d-flex align-items-center">
+          <div className="mb-2 d-flex align-items-center">
             <span className="h3 pr-18">Workspace 1</span>
             <a href="£" type="button">
               <img
@@ -19,6 +19,27 @@ function WorkSpace() {
           </div>
           <div>
             <span className="h5 font-weight-normal">Defualt Workspace</span>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="d-flex justify-content-end ">
+            <button type="button" className="btn-add btn btn-primary mb-10">
+              Add new project
+            </button>
+          </div>
+
+          <div className="d-flex justify-content-end align-items-center">
+            <button
+              type="button"
+              className="share-btn btn b btn-sm btn-primary d-flex align-items-center"
+            >
+              Share
+              <img
+                className="user-icon ml-2"
+                src="https://i.ibb.co/mTy0zHk/user-edited.png"
+                alt="user icon"
+              />
+            </button>
           </div>
         </div>
       </section>
