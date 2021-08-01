daf63ffcf205e13c8b16add90fad8bf17bd480e7
Alaa Alaff <travel98_alaff@hotmail.com>
2021-08-01 17:34:03 +0300

    workSpace bg & card first step styled at DisapleWorkshops component

--- src/components/DisapleWorkShops.js
+++ src/components/DisapleWorkShops.js
@@ -4,7 +4,22 @@ import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function DisapleWorkShops() {
-  return;
+  return (
+    <Container>
+      <section className="mt-30">
+        <div className=" py-5 px-5 shadow border-0">
+          <div className="row d-flex">
+            <div className="col-sm-3 mb-5">
+              <div className="card-bg d-flex align-items-center justify-content-center card mb-1">
+                <span>logo</span>
+              </div>
+              <span className="">Dietitian Project</span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Container>
+  );
 }
 
 export default DisapleWorkShops;
