b50c1d3fd2d7d0a68453985aa00319f9bbc432cd
Alaa Alaff <travel98_alaff@hotmail.com>
2021-08-03 02:23:43 +0300

    create reducer function in redux

---
+++ src/store/redux.js
@@ -0,0 +1,12 @@
+import { createStore } from "redux";
+
+const initialStates = {  };
+
+// Redducer Function
+const reducer = (state = initialStates, action) => {
+  
+};
+
+const store = createStore(reducer);
+
+export default store;
