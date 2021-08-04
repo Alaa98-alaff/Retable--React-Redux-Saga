c7b851b65212fa5963d5e50ac3dbf40898b91b04
Alaa Alaff <travel98_alaff@hotmail.com>
2021-08-05 01:30:52 +0300

    import projectData reducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,9 @@ import { combineReducers, createStore } from "redux";
 
 import modalAppearance from "./modalAppearance";
 import modalTitle from "./modalName";
+import projectData from "./projectData";
 
-const reducers = combineReducers({ modalAppearance, modalTitle });
+const reducers = combineReducers({ modalAppearance, modalTitle, projectData });
 
 const store = createStore(reducers);
 
