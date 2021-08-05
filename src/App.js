ee574a83eb54d7674a206d61773c58c5526144cb
Alaa Alaff <travel98_alaff@hotmail.com>
2021-08-05 15:38:40 +0300

    test display the data in ProjectTable Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import { Route, Switch } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./pages/HomePage.page";
 import ProjectTablePage from "./pages/ProjectTable.page";
-import { getProjData } from "./actions";
 
 function App() {
-  const dispatch = useDispatch();
-  // const data = useSelector((state) => state.projectData.data);
-
-  useEffect(() => {
-    dispatch(getProjData());
-  }, []);
-
   return (
     <div>
       <Switch>
--- src/pages/ProjectTable.page.js
+++ src/pages/ProjectTable.page.js
@@ -1,6 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
+import { getProjData } from "../actions";
+
 function ProjectTablePage() {
+  const dispatch = useDispatch();
+
+  const data = useSelector((state) => state.projectData.data.data?.data);
+
+  console.log(data);
+
+  useEffect(() => {
+    dispatch(getProjData());
+  }, []);
+
   return (
     <div>
+      {data && data.map((data, i) => <p key={i}>{data}</p>)}
       <h1>Project Table page</h1>
     </div>
   );
