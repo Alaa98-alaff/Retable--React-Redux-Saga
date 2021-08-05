1919cb5ad6e49e528bf7530d91802c733265307f
Alaa Alaff <travel98_alaff@hotmail.com>
2021-08-05 18:03:24 +0300

    handle all the data to the table in projectTable PAge

--- src/pages/ProjectTable.page.js
+++ src/pages/ProjectTable.page.js
@@ -7,12 +7,23 @@ import { getProjData } from "../actions";
 function ProjectTablePage() {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.projectData.data.data?.data);
+  const loading = useSelector((state) => state.projectData.loading);
+  const error = useSelector((state) => state.projectData.error);
 
   let tableData = [];
 
-  for (let i = 0; i < data?.length; i++) {
-    tableData.push([{ value: data[i][3] }, { value: data[i][4] }]);
-  }
+  const dataHandler = () => {
+    for (let i = 0; i < data?.length; i++) {
+      tableData.push([
+        { value: data[i][3] },
+        { value: data[i][4] },
+        { value: data[i][5] },
+        { value: data[i][6] },
+      ]);
+    }
+  };
+
+  dataHandler();
 
   useEffect(() => {
     dispatch(getProjData());
@@ -21,6 +32,8 @@ function ProjectTablePage() {
   return (
     <div>
       <h1>Project Table page</h1>
+      {loading && <h2>Loading...</h2>}
+      {error && !loading && <h2>{error}</h2>}
       {data && <Spreadsheet data={tableData} />}
     </div>
   );
