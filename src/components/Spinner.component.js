f5aaeb66085de48133512794345988681bf61752
Alaa Alaff <travel98_alaff@hotmail.com>
2021-08-05 18:36:49 +0300

    SpinnerComponent has been created

---
+++ src/components/Spinner.component.js
@@ -0,0 +1,29 @@
+import { Button, Spinner } from "react-bootstrap";
+
+function SpinnerComponent() {
+  return (
+    <Button
+      variant="primary"
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        marginTop: "-50px",
+        marginLeft: "-50px",
+      }}
+      disabled
+    >
+      <Spinner
+        style={{ marginRight: "10px" }}
+        as="span"
+        animation="border"
+        size="sm"
+        role="status"
+        aria-hidden="true"
+      />
+      Loading...
+    </Button>
+  );
+}
+
+export default SpinnerComponent;
