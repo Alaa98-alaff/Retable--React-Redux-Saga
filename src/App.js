ecbb5d9bbd3b73ed92478b6e81b9a21a5513221b
Alaa Alaff <travel98_alaff@hotmail.com>
2021-08-05 03:08:09 +0300

    get Project Data on reload page -Test-

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Route, Switch } from "react-router-dom";
-
-import HomePage from "./pages/HomePage.page";
-import ProjectTablePage from "./pages/ProjectTable.page";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from "./pages/HomePage.page";
+import ProjectTablePage from "./pages/ProjectTable.page";
+import { getProjData } from "./actions";
 
 function App() {
+  const dispatch = useDispatch();
+  // const data = useSelector((state) => state.projectData.data);
+
+  useEffect(() => {
+    dispatch(getProjData());
+  }, []);
+
   return (
     <div>
       <Switch>
